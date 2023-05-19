50683b1545ec61d54d776e42c02eb3d714c6383a
Irene Egler <irene.egler@gmail.com>
2023-05-19 10:59:51 -0300

    Add bd

--- Controllers/EmprestimoController.cs
+++ Controllers/EmprestimoController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EmprestimoLivro.Data;
+using EmprestimoLivro.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EmprestimoLivro.Controllers
 {
     public class EmprestimoController : Controller
     {
+        readonly private ApplicationDbContext _db;
+        public EmprestimoController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<EmprestimosModel> emprestimos = _db.Emprestimos;
+            return View(emprestimos);
         }
     }
 }
